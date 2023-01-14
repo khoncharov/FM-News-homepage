d6f9fe44cb9efbe47785d2de03b8df46df19eaa7
Konstantin Honcharov <k.honcharov@gmail.com>
2023-01-14 22:13:43 +0300

    feat: add menu initial layout

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-import "./style.scss";
+import './style.scss';
+
+const openMenuBtn = document.querySelector('#btn-menu-open') as HTMLButtonElement;
+const closeMenuBtn = document.querySelector('#btn-menu-close') as HTMLButtonElement;
+const menuContainer = document.querySelector('#mobile-navigation') as HTMLElement;
+
+openMenuBtn.addEventListener('click', () => {
+  menuContainer.classList.toggle('mobile-navigation_active');
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  menuContainer.classList.toggle('mobile-navigation_active');
+});
