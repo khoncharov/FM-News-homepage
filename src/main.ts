3d37dec5a580212fae8ba916c80b36199a4a9204
Konstantin Honcharov <k.honcharov@gmail.com>
2023-01-15 20:10:52 +0300

    refactor: rework mobile menu

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import './style.scss';
 
 const openMenuBtn = document.querySelector('#btn-menu-open') as HTMLButtonElement;
 const closeMenuBtn = document.querySelector('#btn-menu-close') as HTMLButtonElement;
-const menuContainer = document.querySelector('#mobile-navigation') as HTMLElement;
 
-openMenuBtn.addEventListener('click', () => {
-  menuContainer.classList.toggle('mobile-navigation_active');
-});
+const backdrop = document.querySelector('.backdrop') as HTMLElement;
+const mobileMenu = document.querySelector('.mobile-menu') as HTMLElement;
 
-closeMenuBtn.addEventListener('click', () => {
-  menuContainer.classList.toggle('mobile-navigation_active');
-});
+const toggleMobileMenu = () => {
+  backdrop.classList.toggle('backdrop_hidden');
+  mobileMenu.classList.toggle('mobile-menu_hidden');
+};
+
+openMenuBtn.addEventListener('click', toggleMobileMenu);
+
+closeMenuBtn.addEventListener('click', toggleMobileMenu);
+
+backdrop.addEventListener('click', toggleMobileMenu);
